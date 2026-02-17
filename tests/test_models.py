461df753c505c28612c770f09fcc4f9a4eb42daf
Shaira Torio <[shairatorio08@gmail.com>
2026-02-16 22:16:16 -0500

    Added test_read_a_product test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,17 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    def test_read_a_product(self):
+        """ It should Read a product """
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+
+        # Fetch it back
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description , product.description)
+        self.assertEqual(found_product.price, product.price)
